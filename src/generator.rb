f8690d596cc022e0ccbac5520db06a2e9dbdff5d
n4o847 <22975590+n4o847@users.noreply.github.com>
2018-12-24 01:41:48 +0900

    Add _move, sp_const, and sp_not

--- src/generator.rb
+++ src/generator.rb
@@ -8,6 +8,10 @@ module Nazuki
       @out << s
     end
 
+    def _return
+      @out
+    end
+
     def _inc(n = 1)
       _raw(n.positive? ? "+" * n : "-" * -n)
     end
@@ -36,6 +40,44 @@ module Nazuki
 
     def _put
       _raw(".")
+    end
+
+    # 引数は { 相対位置 => 何倍するか } の Hash
+    # 位置は変わらない
+    def _move(dst)
+      _loop do
+        _dec
+        dst.each do |p, a|
+          _right(p)
+          _inc(a)
+          _left(p)
+        end
+      end
+    end
+
+    def sp_const(n)
+      if n & 0xffffffff != n
+        warn("out of range")
+      end
+      _inc
+      32.times do |i|
+        _right
+        _inc(n[i])
+      end
+      _right
+    end
+
+    def sp_not
+      32.times do
+        _inc
+        _left
+        _move({ 1 => -1 })
+      end
+      32.times do
+        _right
+        _move({ -1 => 1 })
+      end
+      _return
     end
   end
 end
