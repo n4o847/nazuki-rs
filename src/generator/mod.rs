85b136c603181d6356ac0c16d893ea3abbb4316e
n4o847 <yuu.miu.0925@gmail.com>
2019-04-20 18:03:16 +0900

    Add generator's basic methods

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -10,6 +10,8 @@ enum BfCmd {
     Put,
 }
 
+struct Ptr(i32);
+
 struct Generator {
     cmds: Vec<BfCmd>,
 }
@@ -20,18 +22,19 @@ impl Generator {
     }
 
     fn build(&mut self) -> String {
-        let mut result = "".to_string();
+        let mut result = String::new();
         for cmd in &self.cmds {
-            match cmd {
-                BfCmd::Inc => result.push('+'),
-                BfCmd::Dec => result.push('-'),
-                BfCmd::Right => result.push('>'),
-                BfCmd::Left => result.push('<'),
-                BfCmd::Open => result.push('['),
-                BfCmd::Close => result.push(']'),
-                BfCmd::Get => result.push(','),
-                BfCmd::Put => result.push('.'),
-            }
+            let ch = match cmd {
+                BfCmd::Inc => '+',
+                BfCmd::Dec => '-',
+                BfCmd::Right => '>',
+                BfCmd::Left => '<',
+                BfCmd::Open => '[',
+                BfCmd::Close => ']',
+                BfCmd::Get => ',',
+                BfCmd::Put => '.',
+            };
+            result.push(ch);
         }
         result
     }
@@ -39,20 +42,20 @@ impl Generator {
     fn raw(&mut self, code: &str) {
         for c in code.chars() {
             match c {
-                '+' => self.raw_inc(),
-                '-' => self.raw_dec(),
-                '>' => self.raw_right(),
-                '<' => self.raw_left(),
-                '[' => self.raw_open(),
-                ']' => self.raw_close(),
-                ',' => self.raw_get(),
-                '.' => self.raw_put(),
+                '+' => self.bf_inc(),
+                '-' => self.bf_dec(),
+                '>' => self.bf_right(),
+                '<' => self.bf_left(),
+                '[' => self.bf_open(),
+                ']' => self.bf_close(),
+                ',' => self.bf_get(),
+                '.' => self.bf_put(),
                 _ => {}
             }
         }
     }
 
-    fn raw_inc(&mut self) {
+    fn bf_inc(&mut self) {
         if self.cmds.last() == Some(&BfCmd::Dec) {
             self.cmds.pop();
         } else {
@@ -60,7 +63,7 @@ impl Generator {
         }
     }
 
-    fn raw_dec(&mut self) {
+    fn bf_dec(&mut self) {
         if self.cmds.last() == Some(&BfCmd::Inc) {
             self.cmds.pop();
         } else {
@@ -68,7 +71,7 @@ impl Generator {
         }
     }
 
-    fn raw_right(&mut self) {
+    fn bf_right(&mut self) {
         if self.cmds.last() == Some(&BfCmd::Left) {
             self.cmds.pop();
         } else {
@@ -76,7 +79,7 @@ impl Generator {
         }
     }
 
-    fn raw_left(&mut self) {
+    fn bf_left(&mut self) {
         if self.cmds.last() == Some(&BfCmd::Right) {
             self.cmds.pop();
         } else {
@@ -84,20 +87,73 @@ impl Generator {
         }
     }
 
-    fn raw_open(&mut self) {
+    fn bf_open(&mut self) {
         self.cmds.push(BfCmd::Open);
     }
 
-    fn raw_close(&mut self) {
+    fn bf_close(&mut self) {
         self.cmds.push(BfCmd::Close);
     }
 
-    fn raw_get(&mut self) {
+    fn bf_get(&mut self) {
         self.cmds.push(BfCmd::Get);
     }
 
-    fn raw_put(&mut self) {
+    fn bf_put(&mut self) {
         self.cmds.push(BfCmd::Put);
+    }
+
+    fn right(&mut self, x: i32) {
+        for _ in 0..x {
+            self.bf_right();
+        }
+        for _ in x..0 {
+            self.bf_left();
+        }
+    }
+
+    fn left(&mut self, x: i32) {
+        self.right(-x);
+    }
+
+    fn enter(&mut self, p: &Ptr) {
+        self.right(p.0);
+    }
+
+    fn exit(&mut self, p: &Ptr) {
+        self.left(p.0);
+    }
+
+    fn add(&mut self, p: &Ptr, x: i32) {
+        self.enter(p);
+        for _ in 0..x {
+            self.bf_inc();
+        }
+        for _ in x..0 {
+            self.bf_dec();
+        }
+        self.exit(p);
+    }
+
+    fn sub(&mut self, p: &Ptr, x: i32) {
+        self.add(p, -x);
+    }
+
+    fn r#while<F: FnMut(&mut Self)>(&mut self, p: &Ptr, mut block: F) {
+        self.enter(p);
+        self.bf_open();
+        self.exit(p);
+        block(self);
+        self.enter(p);
+        self.bf_close();
+        self.exit(p);
+    }
+
+    fn set(&mut self, p: &Ptr, x: i32) {
+        self.r#while(p, |s| {
+            s.bf_dec();
+        });
+        self.add(p, x);
     }
 }
 
