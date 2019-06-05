7e3432b16b69f646752a88d62317019890923786
n4o847 <yuu.miu.0925@gmail.com>
2019-06-06 01:24:56 +0900

    Show generated code size

--- www/index.js
+++ www/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 
 const $result = document.getElementById("result");
+const $length  = document.getElementById("length");
 
 async function load() {
   const nazuki = await import('nazuki');
 
   $result.value = nazuki.generate();
+  $length.textContent = $result.value.length;
 }
 
 try {
